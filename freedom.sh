490f35ddc0ca1c913884a8c5ca96846e6f41ba71
David Eddlemon <DavidEddlemon@Gmail.com>
2013-10-25 13:21:21 -0500

    Update freedom.sh

--- freedom.sh
+++ freedom.sh
@@ -5,10 +5,14 @@
 echo
 echo
 echo This will modify your build environment to allow for custom GCC use
+echo Some tweaking may need to be involved in attempting this on a custom kernel
 echo
 echo
 
 DIR=$(pwd)
+
+cd $DIR/kernel/htc/vigor-3.0/
+git fetch https://github.com/IAmTheOneTheyCallNeo/vigor_aosp_kernel tachyon && git cherry-pick 231d23f73bb872a7c063ccab8bf59898f6ac24cc
 
 cd $DIR/build/
 git fetch https://github.com/IAmTheOneTheyCallNeo/android_build cm-10.2-freedom && git cherry-pick f3fad78b59704464deabed17e595e8de2e634584
