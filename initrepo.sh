500c3893a38ad5bb5916d42c1173ed4685aaa691
sou7611 <tz.32051@gmail.com>
2013-11-20 19:18:35 -0500

    replace default.xml with initrepo.sh and pull directly from cm
    replace default.xml with initrepo.sh and pull directly from cm

---
+++ initrepo.sh
@@ -0,0 +1,25 @@
+#/bin/bash 
+#
+
+# cd to our source root
+cd ~/Android/cm11.0-rezound
+
+[ -d .repo ] || mkdir .repo
+pushd .repo
+
+[ -d local_manifests ] || mkdir local_manifests
+pushd local_manifests
+
+rm -f local_manifest.xml
+wget https://raw.github.com/vigor/android/cm-11.0/local_manifests/local_manifest.xml
+
+popd
+popd
+
+repo init -b cm-11.0 -u git://github.com/CyanogenMod/android.git
+
+repo sync -j16
+
+pushd vendor/cm
+./get-prebuilts
+popd
