8ee5a920bcd60db4406e0dc94eee50c6df69dfb6
Jonathan Deitch <tz.32051@gmail.com>
2013-12-16 01:47:44 -0500

    local manifest for building hybrid ion/pmem
    Change-Id: Idb10c7dcf9850c1779ec571316954ccbe6c2a8fd

---
+++ initrepo_hybrid.sh
@@ -0,0 +1,26 @@
+#/bin/bash 
+#
+
+[ -d .repo ] || mkdir .repo
+pushd .repo
+
+[ -d local_manifests ] || mkdir local_manifests
+pushd local_manifests
+
+rm -f local_manifest.xml
+wget https://raw.github.com/vigor/android/cm-11.0/local_manifests/local_manifest_hybrid.xml
+mv local_manifest_hybrid.xml local_manifest.xml
+popd
+popd
+
+repo init -b cm-11.0 -u git://github.com/CyanogenMod/android.git
+
+pushd .repo
+    sed -i "s/sync-c=\"true\"//g" manifest.xml
+popd
+
+repo sync -j8
+
+pushd vendor/cm
+./get-prebuilts
+popd
