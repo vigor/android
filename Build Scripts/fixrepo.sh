c3d2f73fcfc7ba923ea907559219baa5a7549e29
Jonathan Deitch <tz.32051@gmail.com>
2014-01-11 15:29:41 -0500

    Update fixrepo.sh
    change git:// to https://

--- Build Scripts/fixrepo.sh
+++ Build Scripts/fixrepo.sh
@@ -1,6 +1,7 @@
 #/bin/bash 
 #
 
+# change to our build root (edit if your build root is elsewhere)
 cd ~/Android/cm11.0-rezound
 
 # reset all git projects
@@ -11,7 +12,7 @@ rm -rf .repo/repo
 rm -rf .repo/manifests .repo/manifests.git .repo/manifest.xml
 
 # re-init the repo
-repo init -b cm-11.0 -u git://github.com/vigor/android.git
+repo init -b cm-11.0 -u https://github.com/vigor/android.git
 
 # remove the sync-c=true line from manifest.xml; this ensures we get SDK 19
 pushd .repo
