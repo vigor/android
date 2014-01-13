207bbd1cddf413080c6d5ee6aaf50d920cd5b78b
Jonathan Deitch <tz.32051@gmail.com>
2014-01-13 12:27:51 -0500

    Update fixrepo.sh
    fixrepo.sh should be initing the CM repo, not the vigor repo

--- Build Scripts/fixrepo.sh
+++ Build Scripts/fixrepo.sh
@@ -12,7 +12,7 @@ rm -rf .repo/repo
 rm -rf .repo/manifests .repo/manifests.git .repo/manifest.xml
 
 # re-init the repo
-repo init -b cm-11.0 -u https://github.com/vigor/android.git
+repo init -b cm-11.0 -u https://github.com/CyanogenMod/android.git
 
 # remove the sync-c=true line from manifest.xml; this ensures we get SDK 19
 pushd .repo
